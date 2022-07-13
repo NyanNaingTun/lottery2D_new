8320bf4eb94f36915b81f43104843d5188cd0824
nyannaingtun <mr.nyannaingtun@gmail.com>
2022-07-13 19:29:41 +0630

    save

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    #time.sleep(180)
+    time.sleep(180)
     return {"message": "Hello World 180"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-
     return {"message": f"Hello {name}"}
